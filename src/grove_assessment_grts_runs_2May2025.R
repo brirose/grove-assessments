d06007f266f4aaf2aede2ae4dba977d0724ad893
klshive <kshive@berkeley.edu>
2025-05-09 12:26:39 -0700

    updated tables and code for grts and grid options

--- src/grove_assessment_grts_runs_2May2025.R
+++ src/grove_assessment_grts_runs_2May2025.R
@@ -9,6 +9,7 @@ library(mapedit)
 #import access
 access <- read.csv(here("data/grove_access.csv"))
 plots_sf <- st_read(here("data/spatial_data/all_plots_groves.shp")) 
+final.strata.shape = st_read(here("data/spatial_data/outputs/final.strata.that.need.new.plots_easier.shp"))
 
 #import strata set needs
 strata.needs1 = read.csv(here("data/plot_needs_by_strata_2May2025_kls.csv"))
@@ -37,7 +38,7 @@ for(i in 1:length(shapes.list)) {
   
        draw_grts <- grts(strat.shp,
                   n_base = plots.needed$NEW.plots.needed, # number of 'base' sites 
-                  n_over = plots.needed$NEW.plots.needed*3,          # nuber of 'oversample' sites
+                  n_over = (plots.needed$NEW.plots.needed)*3,          # nuber of 'oversample' sites
                   mindis = 100, # minimum distance between sites. Look at package details to see how it calculates distance unit
                   maxtry = 100 # sometimes, grts can't fit the draw on it's first attempt. 
             )
@@ -62,10 +63,11 @@ for(i in 1:length(shapes.list)) {
   
   #write .csv of grts draw details, eg. lat/long, site number
   # setwd("C:/Users/dsoderberg/OneDrive - DOI/Desktop/Shive_SEGIpretreatment_grts/csv")
-  write.csv(draw_grts_base_and_over[,c(1,2,4,5,10:13,15:18)], here(paste("outputs/grts/grts_base_and_over_",gsub('.{4}$', '', as.character(shape.is)))))
+  write.csv(draw_grts_base_and_over, here(paste("data/spatial_data/outputs/grts/grts_base_and_over_",gsub('.{4}$', '', as.character(shape.is)),".csv",sep="")))
   
     }
 }
+
 
 ##merge all bo and all over to one shapefile each
 bo.dir <- (here("data/spatial_data/outputs/grts"))
@@ -87,17 +89,17 @@ grts.groves = bo %>%
 
 grts.groves.table = grts.groves %>%
   st_drop_geometry() %>%
-  select(-c(replsite,stratum,wgt,ip,caty,grove,unit_name)) %>%
-  group_by(strt_nm,grove_name,siteuse) %>%
-  summarise(num.grts.plots = length(strt_nm), 
-            NEW.plots.needed = mean(NEW_pl_)) %>%
+  select(-c(replsite,stratum,wgt,ip,caty,grove,unit_name)) %>% 
+  # left_join(strata.needs) %>%
+  group_by(strata_nm,grove_name,siteuse) %>%
+  summarise(num.grts.plots = length(strata_nm)) %>%
   st_drop_geometry() %>%
   mutate(grov_nm = grove_name, study_design = "grts") %>%
-  left_join(plots_sf_summary) %>%
+  # left_join(plots_sf_summary) %>%
   select(-grov_nm)
 View(grts.groves.table)
 # #write out csv
-# write.csv(grts.groves,(here("data/spatial_data/outputs/grts_points_easier_groves_table_summary.csv")))
+# write.csv(grts.groves,(here("outputs/grts_points_easier_groves_table_summary.csv")))
 
 grts.groves = read.csv(here("outputs/grts_points_easier_groves_table_summary.csv"))
 
@@ -105,15 +107,17 @@ grts.groves = read.csv(here("outputs/grts_points_easier_groves_table_summary.csv
 ###########################################################
 ##explore grid approach
 
-grid200 <- needed.strata.plots_easier %>% 
+xgrid200 <- groves %>% 
   st_make_grid(cellsize = c(200,200), what = "centers") %>% # grid of points
   st_as_sf() %>%
-  st_intersection(needed.strata.plots_easier) %>%
+  st_intersection(groves_easier) %>%
+  st_intersection(final.strata.shape) %>%
   mutate(study.design = "grid")
-head(grid200)
+head(xgrid200)
+nrow(xgrid200)
 
-# write_sf(grid200,(here("data/spatial_data/outputs/grid200m_points_easier_groves_4May2025_wstrata.shp")),
-#          driver="ESRI Shapefile", overwrite_layer=TRUE)
+write_sf(xgrid200,(here("data/spatial_data/outputs/grid200m_in_needed_strata_easier.shp")),
+         driver="ESRI Shapefile", overwrite_layer=TRUE)
 
 plots_sf_summary = plots_sf %>%
   group_by(grov_nm) %>%
@@ -121,15 +125,14 @@ plots_sf_summary = plots_sf %>%
   st_drop_geometry()
 
 # grid200 = st_read(here("data/spatial_data/outputs/grid200m_points_easier_groves_4May2025_wstrata.shp"))
-grid.plot.summary = grid200 %>%
+grid.plot.summary = xgrid200 %>%
   st_intersection(groves) %>%
   group_by(strata_nm,grove_name) %>%
-  summarise(num.grid.plots = length(strata_nm), 
-            NEW.plots.needed = mean(NEW.plots.needed)) %>%
+  summarise(num.grid.plots = length(strata_nm)) %>%
   st_drop_geometry() %>%
-  mutate(grov_nm = grove_name, study_design = "grid") %>%
-  left_join(plots_sf_summary) %>%
-  select(-grov_nm)
+  mutate(grov_nm = grove_name, study_design = "grid") #%>%
+  # left_join(plots_sf_summary) %>%
+  # select(-grov_nm)
 View(grid.plot.summary)
 
 write.csv(grid.plot.summary,(here("outputs/grid_points_easier_groves_table_summary.csv")))
